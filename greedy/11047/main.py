102d733594221b240afc0b30636697de356cd1a9
Kite_Z <duswl339@ajou.ac.kr>
2023-12-20 16:39:08 +0900

    solve 11047

---
+++ greedy/11047/main.py
@@ -0,0 +1,15 @@
+N, K = map(int, input().split())
+coins = []
+
+for _ in range(N):
+    coins.append(int(input()))
+
+coins.sort(reverse=True)
+count = 0
+
+for coin in coins:
+    if K >= coin:
+        count += K // coin
+        K -= coin * (K // coin)
+
+print(count)
